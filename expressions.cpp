97c367856c0fe3f92996eff08645089784dabb3c
n-miyamoto <scotsdowneroad@gmail.com>
2021-10-18 00:27:19 +0900

    clang format to expressions.*

--- expressions.cpp
+++ expressions.cpp
@@ -1,26 +1,19 @@
 #include "expressions.hpp"
 
+llvm::Value* NumberExprAST::Accept(CodeGenVisitor& v) { return v.Visit(*this); }
 
-llvm::Value* NumberExprAST::Accept(CodeGenVisitor& v){
+llvm::Value* VariableExprAST::Accept(CodeGenVisitor& v) {
   return v.Visit(*this);
 }
 
-llvm::Value* VariableExprAST::Accept(CodeGenVisitor& v){
+llvm::Value* BinaryExprAST::Accept(CodeGenVisitor& v) { return v.Visit(*this); }
+
+llvm::Value* CallExprAST::Accept(CodeGenVisitor& v) { return v.Visit(*this); }
+
+llvm::Function* PrototypeAST::Accept(CodeGenVisitor& v) {
   return v.Visit(*this);
 }
 
-llvm::Value* BinaryExprAST::Accept(CodeGenVisitor& v){
-  return v.Visit(*this);
-}
-
-llvm::Value* CallExprAST::Accept(CodeGenVisitor& v){
-  return v.Visit(*this);
-}
-
-llvm::Function *PrototypeAST::Accept(CodeGenVisitor &v){
-  return v.Visit(*this);
-}
-
-llvm::Function *FunctionAST::Accept(CodeGenVisitor &v){
+llvm::Function* FunctionAST::Accept(CodeGenVisitor& v) {
   return v.Visit(*this);
 }
